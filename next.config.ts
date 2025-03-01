338636edceb0bab0c8bfee6d3bbf4d0d916b6c5e
Ahmed Kamel <ahmed.kamel@arabiansystems.com>
2025-03-01 23:23:01 +0200

    edit configuration

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,6 @@ const nextConfig = {
     domains: ["fakestoreapi.com"],
     unoptimized: false,
   },
-  experimental: {
-    optimizeCss: true,
-    optimizePackageImports: ["framer-motion"],
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
 };
 
 module.exports = nextConfig;
